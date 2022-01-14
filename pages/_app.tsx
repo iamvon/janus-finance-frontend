3fa6958dcdcd12b5655d2e51c988c2718a52fa59
Pham Minh Tuan <tuanpm@foobla.com>
2022-01-14 16:12:00 +0700

    remove antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import { FC } from 'react';
 import { ContextProvider } from '../components/ContextProvider';
 
 // Use require instead of import since order matters
-require('antd/dist/antd.dark.less');
-require('@solana/wallet-adapter-ant-design/styles.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 
