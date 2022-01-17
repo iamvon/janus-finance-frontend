af91941ba4e4ffa048be193c38b3091037080364
Vương Thành <vuongthanh148@gmail.com>
2022-01-17 23:47:20 +0700

    update route

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,13 @@ import { useAutoConnect } from '../components/AutoConnectProvider';
 import { RequestAirdrop } from '../components/RequestAirdrop';
 import { SendTransaction } from '../components/SendTransaction';
 import { SignMessage } from '../components/SignMessage';
+import Link from 'next/link'
 
 const Index: NextPage = () => {
     const { autoConnect, setAutoConnect } = useAutoConnect();
 
     return (
-        <div>
+        <div className='flex flex-col space-y-4 justify-start items-start'>
             <div>
                 Connect Button
                 <ReactUIWalletConnectButton />
@@ -34,6 +35,9 @@ const Index: NextPage = () => {
             <RequestAirdrop />
             <SendTransaction />
             <SignMessage />
+            <Link href='/solana'>Solana</Link>
+            <Link href='/wormhole'>Wormhole</Link>
+            <Link href='/test'>Test Pyth price</Link>
         </div>
     );
 };
